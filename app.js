890c65c20d34024f3861f457a05af23f0ca6385b
Victor <victorv24@gmail.com>
2021-05-16 17:18:38 -0700

    Snake is moving on it's own.

--- app.js
+++ app.js
@@ -4,13 +4,13 @@ window.onload = () => {
 	const framesPerSecond = 30;
 
 	const APPLE = {
-		size: 9,
+		size: 5,
 		color: 'red',
 		points: 1,
 	};
 
 	const SNAKE = {
-		size: 20,
+		size: 10,
 		color: 'green',
 		travelSpeed: 5,
 		coordinates: {
@@ -18,6 +18,12 @@ window.onload = () => {
 			yPos: canvas.width / 2.1,
 		},
 	};
+
+	// let snakePosition = {
+	// 	// initialize snake position
+	// 	xPos: canvas.width / 2,
+	// 	yPos: canvas.width / 2,
+	// };
 
 	const randomEvenNumber = (canvasSize) => {
 		let randomNumber = Math.floor(Math.random() * canvasSize);
@@ -38,19 +44,28 @@ window.onload = () => {
 	};
 
 	const drawSnake = (x, y) => {
+		SNAKE.coordinates.yPos -= 2;
+		console.log(SNAKE.coordinates.yPos);
+
 		canvasContext.beginPath();
 		canvasContext.fillStyle = SNAKE.color;
-		canvasContext.rect(x, y, SNAKE.size, SNAKE.size);
+		// canvasContext.rect(x, y, SNAKE.size, SNAKE.size);
+		canvasContext.rect(
+			SNAKE.coordinates.xPos,
+			SNAKE.coordinates.yPos,
+			SNAKE.size,
+			SNAKE.size
+		);
 		canvasContext.fill();
 		canvasContext.closePath();
 	};
 
-	const drawApple = (canvasContext) => {
+	const drawApple = (x, y) => {
 		canvasContext.beginPath();
 		canvasContext.fillStyle = APPLE.color;
 		canvasContext.arc(
-			randomEvenNumber(canvas.width - 5),
-			randomEvenNumber(canvas.width - 5),
+			canvas.width - 5,
+			canvas.width - 5,
 			APPLE.size,
 			0,
 			Math.PI * 2,
@@ -60,50 +75,48 @@ window.onload = () => {
 		canvasContext.closePath();
 	};
 
-	drawApple(canvasContext);
-	drawSnake(SNAKE.coordinates.xPos, SNAKE.coordinates.yPos);
-
-	// game initialized and scanning the canvas for human interaction
-	setInterval(() => {
-		window.addEventListener('keydown', handleKeyDownEvent);
-	}, 1000 / framesPerSecond);
-
-	const handleKeyDownEvent = (e) => {
-		snakeDirection(e);
-	};
-
-	const snakeDirection = (e) => {
+	const checkSnakeDirection = (e) => {
+		// debugger;
 		switch (e.key) {
 			case 'ArrowUp':
-				drawBackground();
-				drawSnake(
-					SNAKE.coordinates.xPos,
-					(SNAKE.coordinates.yPos -= SNAKE.travelSpeed)
-				);
+				SNAKE.coordinates.yPos -= SNAKE.travelSpeed;
+				// snakePosition.yPos -= SNAKE.travelSpeed;
+				console.log(SNAKE.coordinates.yPos);
+
 				break;
 			case 'ArrowRight':
-				drawBackground();
-				drawSnake(
-					(SNAKE.coordinates.xPos += SNAKE.travelSpeed),
-					SNAKE.coordinates.yPos
-				);
+				SNAKE.coordinates.xPos += SNAKE.travelSpeed;
 				break;
 			case 'ArrowDown':
-				drawBackground();
-				drawSnake(
-					SNAKE.coordinates.xPos,
-					(SNAKE.coordinates.yPos += SNAKE.travelSpeed)
-				);
+				SNAKE.coordinates.yPos += SNAKE.travelSpeed;
 				break;
 			case 'ArrowLeft':
-				drawBackground();
-				drawSnake(
-					(SNAKE.coordinates.xPos -= SNAKE.travelSpeed),
-					SNAKE.coordinates.yPos
-				);
+				SNAKE.coordinates.xPos -= SNAKE.travelSpeed;
 				break;
 			default:
 				break;
 		}
 	};
+
+	const keyEventListener = (e) => {
+		checkSnakeDirection(e);
+	};
+
+	// game initialized and scanning the canvas for human interaction
+	setInterval(() => {
+		console.log('beep ... beep ...');
+
+		// (heartbeat)
+		drawBackground();
+		drawSnake();
+		drawSnake();
+		drawSnake();
+		drawApple(canvas.width - 5, canvas.width - 5);
+
+		// move the snake
+		// check to see if snake is eating apple
+		// check if snake is touching boundaries
+	}, 1000 / framesPerSecond);
+
+	window.addEventListener('keydown', keyEventListener);
 };
