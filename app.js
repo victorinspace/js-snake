3288bc1367cf389063cef78fb93a9fa31021dee8
Victor <victorv24@gmail.com>
2021-05-23 08:43:34 -0700

    Moves score tracker outside of canvas.

--- app.js
+++ app.js
@@ -1,180 +1,189 @@
 window.onload = () => {
   const DEBUG = true;
 
-	let canvas = document.getElementById('gameCanvas');
-	let canvasContext = canvas.getContext('2d');
-	let playerScore = 0;
-	let framesPerSecond = 1000 / 30;
+  // GRID = 20px
+  let canvas = document.getElementById( 'gameCanvas' );
+  let canvasContext = canvas.getContext( '2d' );
+  let playerScore = 0;
+  let framesPerSecond = 1000 / 30;
 
-  const randomEvenNumber = (canvasSize) => {
-		let randomNumber = Math.floor(Math.random() * canvasSize);
-		if (randomNumber % 2 === 1) {
-			randomNumber++;
-		}
-		return randomNumber;
-	};
+  const randomGridPosition = () => {
+    let randomNumber = ( Math.floor( Math.random() * ( canvas.width - 1 + 1 ) + 1 ) ) * 20;
+
+    // if ( randomNumber % 2 === 1 ) {
+    //   randomNumber++;
+    // }
+
+    return randomNumber;
+  };
 
   let score = {
     total: 0
   }
 
-	const APPLE = {
-		size: 10,
-		color: 'red',
-		points: 1,
+  const APPLE = {
+    size: 10,
+    color: 'red',
+    points: 1,
     coordinates: {
-      xPos: randomEvenNumber(canvas.width),
-      yPos: randomEvenNumber(canvas.width)
+      xPos: randomGridPosition(),
+      yPos: randomGridPosition()
     }
-	};
+  };
 
-	const SNAKE = {
-		size: 20,
-		color: 'green',
-		travelSpeed: 20,
-		direction: 'right',
+  const SNAKE = {
+    size: 20,
+    color: 'green',
+    travelSpeed: 5,
+    direction: 'right',
     body: [
-      { xPos: 80, yPos: 200 },
       { xPos: 60, yPos: 200 },
       { xPos: 40, yPos: 200 },
       { xPos: 20, yPos: 200 },
       { xPos: 0, yPos: 200 },
     ]
-	};
-  
+  };
 
   const stopGame = () => {
-    clearInterval(gameInterval);
+    clearInterval( gameInterval );
   }
 
-	const drawBackground = () => {
-		canvasContext.beginPath();
-		canvasContext.fillStyle = 'black';
-		canvasContext.rect(0, 0, canvas.width, canvas.width);
-		canvasContext.fill();
-		canvasContext.closePath();
-	};
+  const drawBackground = () => {
+    canvasContext.beginPath();
+    canvasContext.fillStyle = 'black';
+    canvasContext.rect( 0, 0, canvas.width, canvas.width );
+    canvasContext.fill();
+    canvasContext.closePath();
+  };
 
   const moveSnake = () => {
     let snakeDirection = SNAKE.direction;
-    const snakeCopy = SNAKE.body.map(snakePart => Object.assign({}, snakePart))
+    const snakeCopy = SNAKE.body.map( snakePart => Object.assign( {}, snakePart ) )
 
-		if (snakeDirection === 'up') {
-			SNAKE.body[0].yPos -= SNAKE.travelSpeed;
-		} else if (snakeDirection === 'right') {
-			SNAKE.body[0].xPos += SNAKE.travelSpeed;
-		} else if (snakeDirection === 'down') {
-			SNAKE.body[0].yPos += SNAKE.travelSpeed;
-		} else if (snakeDirection === 'left') {
-			SNAKE.body[0].xPos -= SNAKE.travelSpeed;
-		}
+    if ( snakeDirection === 'up' ) {
+      SNAKE.body[0].yPos -= SNAKE.travelSpeed;
+    } else if ( snakeDirection === 'right' ) {
+      SNAKE.body[0].xPos += SNAKE.travelSpeed;
+    } else if ( snakeDirection === 'down' ) {
+      SNAKE.body[0].yPos += SNAKE.travelSpeed;
+    } else if ( snakeDirection === 'left' ) {
+      SNAKE.body[0].xPos -= SNAKE.travelSpeed;
+    }
 
     // set position for snake body (not head)
-    for (let i = 1; i < SNAKE.body.length; i++) {
+    for ( let i = 1; i < SNAKE.body.length; i++ ) {
       SNAKE.body[i] = snakeCopy[i - 1];
     }
   }
 
-	const drawSnake = (x, y) => {
-		canvasContext.beginPath();
-		canvasContext.fillStyle = SNAKE.color;
-		canvasContext.rect(x, y, SNAKE.size, SNAKE.size);
-		canvasContext.fill();
-		canvasContext.closePath();
-	};
+  const drawSnake = ( x, y ) => {
+    canvasContext.beginPath();
+    canvasContext.fillStyle = SNAKE.color;
+    canvasContext.rect( x, y, SNAKE.size, SNAKE.size );
+    canvasContext.fill();
+    canvasContext.closePath();
+  };
 
-	const drawApple = () => {
-		canvasContext.beginPath();
-		canvasContext.fillStyle = APPLE.color;
-		canvasContext.arc(
-      APPLE.coordinates.xPos, APPLE.coordinates.yPos, 
-      APPLE.size, 0, 
-      Math.PI * 2);
-		canvasContext.fill();
-		canvasContext.closePath();
-	};
+  const drawApple = () => {
+    canvasContext.beginPath();
+    canvasContext.fillStyle = APPLE.color;
+    canvasContext.arc(
+      APPLE.coordinates.xPos, APPLE.coordinates.yPos,
+      APPLE.size, 0,
+      Math.PI * 2 );
+    canvasContext.fill();
+    canvasContext.closePath();
+  };
 
   const drawCurrentScore = () => {
     canvasContext.font = '10px Arial';
     canvasContext.fillStyle = 'white';
-    canvasContext.fillText(`Score: ${playerScore}`, 6, 13);
+    canvasContext.fillText( `Score: ${playerScore}`, 6, 13 );
+
+    document.getElementById( 'player-score' ).append( playerScore );
   }
 
   const doesSnakeEatApple = () => {
-    if (SNAKE.body[0].xPos === APPLE.coordinates.xPos &&
+    console.log( APPLE.coordinates.xPos, APPLE.coordinates.yPos );
+    console.log( SNAKE.body[0].xPos, SNAKE.body[0].yPos )
+
+    if ( SNAKE.body[0].xPos === APPLE.coordinates.xPos &&
       SNAKE.body[0].yPos === APPLE.coordinates.yPos ) {
-        console.log(`Apple eaten! The snake was at ${SNAKE.body[0].xPos}, ${SNAKE.body[0].yPos}`);
-        playerScore++;
-        APPLE.coordinates.xPos = randomEvenNumber(canvas.width);
-        APPLE.coordinates.yPos = randomEvenNumber(canvas.width);
-		}
+      playerScore++;
+      APPLE.coordinates.xPos = randomGridPosition();
+      APPLE.coordinates.yPos = randomGridPosition();
+      drawCurrentScore();
+    }
+
+
   }
 
-// *** == Debuging Env == *** //
-  if (DEBUG === true) {
-    APPLE.coordinates.xPos = 60;
+  // *** == Debuging Env == *** //
+  if ( DEBUG === true ) {
+    APPLE.coordinates.xPos = 200;
     APPLE.coordinates.yPos = 200;
     framesPerSecond = 500;
   }
 
 
-// *** == GAME INITIALIZED == *** //
-	let gameInterval = setInterval(() => {
+  // *** == GAME INITIALIZED == *** //
+  let gameInterval = setInterval( () => {
     moveSnake();
     doesSnakeEatApple();
 
-		// check if snake hits a wall
-		if (SNAKE.body[0].yPos === 0 || SNAKE.body[0].yPos === canvas.width) {
-			console.log('Wall Hit! Game Over!');
-      console.log(`snake: ${SNAKE.body[0].xPos}, ${SNAKE.body[0].yPos}`);
-      
+    // check if snake hits a wall
+    if ( SNAKE.body[0].yPos === 0 || SNAKE.body[0].yPos === canvas.width ) {
+
+      console.log( 'Wall Hit! Game Over!' );
+      console.log( `snake: ${SNAKE.body[0].xPos}, ${SNAKE.body[0].yPos}` );
+
       stopGame();
-			// check player high score
-		} else if (SNAKE.body[0].xPos === 0 || SNAKE.body[0].xPos === canvas.width) {
-			console.log('Wall Hit! Game Over!');
-      console.log(`snake: ${SNAKE.body[0].xPos}, Y: ${SNAKE.body[0].yPos}`);
+      // check player high score
+
+    } else if ( SNAKE.body[0].xPos === 0 || SNAKE.body[0].xPos === canvas.width ) {
+
+      console.log( 'Wall Hit! Game Over!' );
+      console.log( `snake: ${SNAKE.body[0].xPos}, Y: ${SNAKE.body[0].yPos}` );
       stopGame();
-			// check player high score
-		} else {
-			drawBackground();
-			
-      // drawSnake(SNAKE.body[0].xPos, SNAKE.body[0].yPos);
-      // drawSnake(SNAKE.body[1].xPos, SNAKE.body[1].yPos);
-      // drawSnake(SNAKE.body[2].xPos, SNAKE.body[2].yPos);
-      // drawSnake(SNAKE.body[3].xPos, SNAKE.body[3].yPos);
-      
-      for (let i = 0; i < SNAKE.body.length; i++) {
-        drawSnake(SNAKE.body[i].xPos, SNAKE.body[i].yPos);
+      // check player high score
+
+    } else {
+
+      drawBackground();
+
+      for ( let i = 0; i < SNAKE.body.length; i++ ) {
+        drawSnake( SNAKE.body[i].xPos, SNAKE.body[i].yPos );
       }
-      
+
       drawApple();
-      console.log(`apl: ${APPLE.coordinates.xPos}, ${APPLE.coordinates.xPos}`);
-      
-      drawCurrentScore();
-		}
+      // console.log( `apl: ${APPLE.coordinates.xPos}, ${APPLE.coordinates.xPos}` );
 
-	}, framesPerSecond);
+      // drawCurrentScore();
+
+    }
+
+  }, framesPerSecond );
 
 
-	window.addEventListener('keydown', (e) => {
-		e.preventDefault();
-		switch (e.key) {
-			case 'ArrowUp':
-				SNAKE.direction = 'up';
-				break;
-			case 'ArrowRight':
-				SNAKE.direction = 'right';
-				break;
-			case 'ArrowDown':
-				SNAKE.direction = 'down';
-				break;
-			case 'ArrowLeft':
-				SNAKE.direction = 'left';
-				break;
-			default:
-				break;
-		}
-	});
+  window.addEventListener( 'keydown', ( e ) => {
+    e.preventDefault();
+    switch ( e.key ) {
+      case 'ArrowUp':
+        SNAKE.direction = 'up';
+        break;
+      case 'ArrowRight':
+        SNAKE.direction = 'right';
+        break;
+      case 'ArrowDown':
+        SNAKE.direction = 'down';
+        break;
+      case 'ArrowLeft':
+        SNAKE.direction = 'left';
+        break;
+      default:
+        break;
+    }
+  } );
 
 };
 
